cce6cc527c61bc700fa4938142b12f23330f3c6d
Tomas Padilla <padillatomasagustin@gmail.com>
2025-02-07 11:57:04 +0100

    develop - Base Project

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: {
+		default: "TAG Board",
+		template: "%s | TAG Board",
+	},
+	description:
+		"TAG Board es una plataforma para gestionar proyectos al estilo Trello/Jira, ideal para organizar tus tareas, equipos y flujo de trabajo.",
+	keywords: ["Gestión de Proyectos", "Kanban", "Trello", "Scrum", "Agile"],
+	openGraph: {
+		type: "website",
+		locale: "es_ES",
+		url: "https://tag-board-ui.vercel.app/",
+		title: "TAG Board | Gestiona tus proyectos de forma ágil",
+		description:
+			"TAG Board es la herramienta ideal para organizar tareas y proyectos al estilo Trello/Jira. Perfecto para equipos ágiles y metodología Scrum.",
+	},
+	icons: {
+		icon: "./favicon.ico",
+	},
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+		</html>
+	);
+}
