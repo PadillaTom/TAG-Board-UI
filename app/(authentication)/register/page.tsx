ed8089b2de0c4e3fd9e648df95698a0decf87270
Tomas Padilla <padillatomasagustin@gmail.com>
2025-02-08 09:06:03 +0100

    [TBF-3] Basic pages and modify Settings VSCODE, apply format to all docs

--- app/(authentication)/register/page.tsx
+++ app/(authentication)/register/page.tsx
@@ -1,10 +1,10 @@
 export default function RegisterPage() {
-	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-				<h2>TAG Board</h2>
-				<h2>Register</h2>
-			</main>
-		</div>
-	);
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h2>TAG Board</h2>
+        <h2>Register</h2>
+      </main>
+    </div>
+  );
 }
---
+++ components/ui/library/structure/Container.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+
+export const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className: string;
+}) => {
+  return <div className={cn("", className)}>{children}</div>;
+};
