a424d5464cb2228b63f94835f7abd126c4dfdc90
Tomas Padilla <padillatomasagustin@gmail.com>
2025-02-08 17:06:01 +0100

    [TBF-6] Create Constants for Routing

---
+++ constants/routes.ts
@@ -0,0 +1,34 @@
+// ================================
+//  Web App Routes
+// ================================
+
+export const WEBSITE_ROUTES = {
+	HOME: '/',
+	LOGIN: '/login',
+	REGISTER: '/register',
+	ABOUT: '/about',
+	CONTACT: '/contact',
+};
+
+export const USER_ROUTES = {
+	DASHBOARD: '/dashboard',
+	PROFILE: '/dashboard/profile',
+};
+
+export const navLinks = {
+	guest: [
+		{ label: 'Inicio', href: WEBSITE_ROUTES.HOME },
+		{ label: 'Iniciar Sesión', href: WEBSITE_ROUTES.LOGIN },
+		{ label: 'Regístrate!', href: WEBSITE_ROUTES.REGISTER },
+		{ label: 'Acerca de nosotros', href: WEBSITE_ROUTES.ABOUT },
+		{ label: 'Contacto', href: WEBSITE_ROUTES.CONTACT },
+	],
+	user: [
+		{ label: 'Inicio', href: USER_ROUTES.DASHBOARD },
+		{ label: 'Mi Perfil', href: USER_ROUTES.PROFILE },
+	],
+};
+
+// ================================
+//  Env Variables
+// ================================
