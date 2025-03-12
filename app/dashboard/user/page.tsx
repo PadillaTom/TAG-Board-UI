5ff23022be0a4be7bfb06c5b0242f2855a6e3fe0
Tomas Padilla <padillatomasagustin@gmail.com>
2025-03-12 12:11:44 +0100

    [TBF-10] Logout

--- app/dashboard/user/page.tsx
+++ app/dashboard/user/page.tsx
@@ -1,12 +1,16 @@
+'use client';
+
 import Link from 'next/link';
 import { USER_ROUTES } from '../../../constants/routes';
 import { Button } from '../../../components/ui/button';
+import { useAuth } from '../../../context/AuthContext';
 
 const UserDashboardPage = () => {
+	const { logoutUser } = useAuth();
 	return (
 		<div className="flex flex-col gap-4">
 			<Link href={USER_ROUTES.PROFILE}>Mi Perfil</Link>
-			<Button>Salir</Button>
+			<Button onClick={() => logoutUser()}>Salir</Button>
 		</div>
 	);
 };
